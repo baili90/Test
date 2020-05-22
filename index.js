639e7df992c0ad0a7216cb13247ded4895c69e44
朱玉青 <qianshibuwang@yahoo.com>
2020-05-22 18:27:33 +0800

    new sort

---
+++ index.js
@@ -0,0 +1,13 @@
+const text = document.getElementById('text');
+text.onclick = function (e) {
+  console.log('onclick')
+}
+text.onfocus = function (e) {
+  console.log('onfocus')
+}
+text.onmousedown = function (e) {
+  console.log('onmousedown')
+}
+text.onmouseenter = function (e) {
+  console.log('onmouseenter')
+}
